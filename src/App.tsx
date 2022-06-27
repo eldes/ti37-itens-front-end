b391a24086890c402e22934c3f8f96c1f635d487
Eldes <eldes.poliveira@sp.senac.br>
2022-06-27 15:21:36 -0300

    [+] roteamento de páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import UsuariosPage from './pages/Usuarios';
 
 const App = function () {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/usuarios' element={<UsuariosPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
