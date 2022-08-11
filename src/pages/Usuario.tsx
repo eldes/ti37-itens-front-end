1d9d02ff27e1e50ecaabd8b77f4bf8e58b146476
Eldes <github@eldes.com>
2022-08-11 15:05:43 -0300

    [+] Implenta a exibição dos dados na tela Usuário

--- src/pages/Usuario.tsx
+++ src/pages/Usuario.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Usuario from '../models/Usuario';
@@ -9,15 +9,24 @@ const UsuarioPage = function () {
   const id = params.id;
 
   const [usuario, setUsuario] = useState<Usuario>();
+  const [estaCarregando, setEstaCarregando] = useState(false);
+  const [status, setStatus] = useState(200);
 
   const lerUsuarioComSucesso = function(res: AxiosResponse) {
+    setEstaCarregando(false);
+    setStatus(res.status);
     setUsuario(res.data);
   };
 
-  const lerUsuarioComErro = function() {};
+  const lerUsuarioComErro = function(error: AxiosError) {
+    setEstaCarregando(false);
+    if (error.response) {
+      setStatus(error.response.status);
+    }
+  };
 
   const htmlRenderizado = function () {
-    //TODO: Mostrar indicador de carregamento.
+    setEstaCarregando(true);
     axios.get<Usuario>(`http://localhost:4000/api/usuarios/${id}`)
     .then(lerUsuarioComSucesso)
     .catch(lerUsuarioComErro);
@@ -27,12 +36,28 @@ const UsuarioPage = function () {
 
   return (
     <>
-      <h1>{id} - {usuario?.nome}</h1>
-      <ul>
-        <li>Login: {usuario?.login}</li>
-        <li>Senha: {usuario?.senha}</li>
-      </ul>
-      <button>Remover</button>
+      {(estaCarregando) && (
+        <p>Carregando...</p>
+      )}
+      
+      {(status !== 200) && (
+        <p>ERRO ao tentar carregar.</p>
+      )}
+
+      {(status === 404) && (
+        <p>Usuário não cadastrado</p>
+      )}
+
+      {(status === 200) && (
+        <>
+          <h1>{id} - {usuario?.nome}</h1>
+          <ul>
+            <li>Login: {usuario?.login}</li>
+            <li>Senha: {usuario?.senha}</li>
+          </ul>
+          <button>Remover</button>
+        </>
+      )}
     </>
   );
 };
